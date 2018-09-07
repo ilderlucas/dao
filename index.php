ba0c4e7d8e2cbddede39a846d3a95509b7e84300
Lucas Oliveira <luisilderlandiolucas@gmail.com>
2018-09-07 16:40:53 -0300

    criou-se a classe usuário

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php 
 
  require_once("config.php");
-
+/*
  $sql = new Sql();
  $user=$sql->select("SELECT pass FROM autenticacao WHERE id=1");
  echo json_encode($user);
+ */
+
+$user = new Usuario();
+$user->loadById(1);
+echo $user;
+
  ?>
