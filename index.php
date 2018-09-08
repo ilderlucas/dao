13324033643588953ea00f092177e27a06fef9df
Lucas Oliveira <luisilderlandiolucas@gmail.com>
2018-09-08 10:36:07 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -7,8 +7,23 @@
  echo json_encode($user);
  */
 
-$user = new Usuario();
-$user->loadById(1);
-echo $user;
+// carrega apenas um usuário
+//$user = new Usuario();
+//$user->loadById(2);
+ //echo $user;
+
+ // lista de usuários - não funcionou
+ // $lista = Usuario::getList();
+  //echo json_encode($lista);
+ 
+ // pesquisa por por um usuário por uma parte da palavra
+ //$search = Usuario::search("Lu");
+ //echo json_encode($search);
+
+ // carrega usuário usando login e senha
+
+ $usuario = new Usuario();
+ $usuario->login("Lucas Oliveira","12567890");
+ echo $usuario;
 
  ?>
